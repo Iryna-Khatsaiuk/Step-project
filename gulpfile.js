186ee9b1e6c3bcf8e5a1cac5b79ee5906c0bbda3
Andriy Ivanchenko <Clawhammer91@gmail.com>
2023-11-21 20:02:10 +0100

    feat: finished header, heading and about me sections. Faced difficulties with SCSS syntax but solved them with the use of and keyword for media-queries. This commit will follow the commit for the Selected-works section. First attempt to merge my branch to dev-branch and fixing conflicts.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ import gulp from "gulp";
 const { src, dest, watch, series, parallel } = gulp;
 
 import imagemin from "gulp-imagemin";
+import svgo from "imagemin-svgo";
+import svgmin from "gulp-svgmin"; 
 import autoprefixer from "gulp-autoprefixer";
 import csso from "gulp-csso";
 import clean from "gulp-clean";
@@ -23,7 +25,7 @@ const processHtmlTaskHandler = () => {
   return src("./src/index.html")
     .pipe(fileInclude({
       prefix: '@@',
-      basepath: './src', // Set the correct base path to resolve include paths
+      basepath: './src', 
     }))
     .pipe(dest("./dist"))
     .on("end", function () {
@@ -32,6 +34,22 @@ const processHtmlTaskHandler = () => {
     .pipe(browserSync.stream());
 };
 
+const layoutHtmlTaskHandler = () => {
+  console.log("Processing layout HTML task started");
+
+  return src("./src/html/layout/**/*.html")
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: './src', 
+    }))
+    .on("data", function (file) {
+    })
+    // .pipe(dest("./dist"))
+    .on("end", function () {
+      console.log("Layout HTML task completed successfully");
+    })
+    .pipe(browserSync.stream());
+};
 
 const cssTaskHandler = () => {
   console.log("CSS task started");
@@ -56,9 +74,17 @@ const cssTaskHandler = () => {
 };
 
 const imagesTaskHandler = () => {
-  return src("./src/img/**/*.*")
-    .pipe(imagemin())
+  return src(["./src/img/**/*.{jpg,jpeg,png,gif}", "!./src/img/**/*.svg"])
+    .pipe(imagemin([
+      svgo()
+    ]))
     .pipe(dest("./dist/img"));
+};
+
+const svgTaskHandler = () => {
+  return src("./src/img/**/*.svg")
+    .pipe(svgmin())
+    .pipe(dest("./dist/img/"));
 };
 
 const fontTaskHandler = () => {
@@ -77,35 +103,41 @@ const browserSyncTaskHandler = () => {
       baseDir: "./dist",
     },
   });
-
+  watch("./src/img/**/*.{jpg,jpeg,png,gif}").on(
+    "all",
+    series(imagesTaskHandler, browserSync.reload)
+  );
+  watch("./src/img/**/*.svg").on(
+    "all",
+    series(svgTaskHandler, browserSync.reload)
+  );
   watch("./src/scss/**/*.scss").on(
     "all",
     series(cssTaskHandler, browserSync.reload)
   );
   watch("./src/*.html").on(
     "change",
-    series(htmlTaskHandler, processHtmlTaskHandler, browserSync.reload)
+    series(htmlTaskHandler, layoutHtmlTaskHandler, processHtmlTaskHandler, browserSync.reload)
   );
   watch("./src/html/layout/**/*.html").on(
     "all",
-    series(htmlTaskHandler, processHtmlTaskHandler, browserSync.reload)
-  );
-  watch("./src/img/**/*").on(
-    "all",
-    series(imagesTaskHandler, browserSync.reload)
+    series(htmlTaskHandler, layoutHtmlTaskHandler, processHtmlTaskHandler, browserSync.reload)
   );
 };
 
 export const cleaning = cleanDistTaskHandler;
 export const html = htmlTaskHandler;
 export const processHtml = processHtmlTaskHandler;
+export const layoutProcessHtml = layoutHtmlTaskHandler;
 export const css = cssTaskHandler;
 export const font = fontTaskHandler;
 export const images = imagesTaskHandler;
+export const svg = svgTaskHandler;
 
 export const build = series(
   cleanDistTaskHandler,
-  parallel(htmlTaskHandler, cssTaskHandler, fontTaskHandler, imagesTaskHandler)
+  parallel(htmlTaskHandler, cssTaskHandler, fontTaskHandler, imagesTaskHandler, svgTaskHandler)
 );
 
-export const dev = series(build, processHtmlTaskHandler, browserSyncTaskHandler);
+export const dev = series(build, parallel(layoutHtmlTaskHandler, processHtmlTaskHandler, browserSyncTaskHandler));
+
